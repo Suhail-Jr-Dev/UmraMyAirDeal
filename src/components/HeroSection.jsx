7b4247722c9d5895bfcfe520dd0f73201e353db9
Suhail-sJrD <suhailsince2003@gmail.com>
2024-10-22 15:48:41 +0530

    adding video tag

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import '../Styles/HeroSection.css';
 
 // Import your video file. Make sure the path is correct.
-import backgroundVideo from '../assetss/HeroSection/Landscape.mp4'
-
+import backgroundVideo from '../assetss/HeroSection/Landscape.mp4';
 
 function HeroSection() {
     return (
-        <div className='heroSection  py-5 h-[70vh] flex items-center justify-center relative'>
+        <div className='heroSection py-5 h-[70vh] flex items-center justify-center relative'>
             {/* Background Video */}
-            <video autoPlay loop muted className="absolute top-0 left-0 w-full h-full object-cover z-[-1]">
+            <video
+                autoPlay
+                loop
+                muted
+                playsInline
+                className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+            >
                 <source src={backgroundVideo} type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
+
+            
         </div>
     );
 }
